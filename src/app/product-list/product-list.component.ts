17b8676c08550ca50a82c99247b34e95364a9708
Juan Carlos Muro <juancarlos.muro@valtech.nl>
2019-08-02 10:29:14 +0200

    Added "Buy" button to products in the product list

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-list',
@@ -10,12 +11,19 @@ import { products } from '../products';
 export class ProductListComponent {
   products = products;
 
+  constructor(private cartService: CartService) {}
+
   share() {
     window.alert('The product has been shared!');
   }
 
   onNotify() {
     window.alert('You will be notified when the product goes on sale');
+  }
+
+  buy(product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
   }
 }
 
